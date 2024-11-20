b9aad80cf6de19dfbb741dab12aeb584aee4e5b5
gdmst <beatbenz1501@gmail.com>
2024-11-20 03:06:32 -0600

    retrieve Google news

--- Google_News_Sexbot.py
+++ Google_News_Sexbot.py
@@ -1,29 +1,36 @@
 from GoogleNews import GoogleNews
 import pandas as pd
 from pathlib import Path  
+import calendar
 
 googlenews = GoogleNews()
 
-for year in range(2010, 2025):
-    print(year)
-
-    start_date = '01/01/'+str(year)
-    end_date = '12/31/'+str(year)
-    googlenews=GoogleNews(start=start_date,end=end_date)
-    googlenews.search('sex robot')
-    googlenews.set_encode('utf-8')
-    googlenews.get_news('sex robot')
-
-    result=googlenews.result()
-    df=pd.DataFrame(result)
+for year in range(2010, 2011):
     
-    # for i in range(2,20):
-    #     googlenews.getpage(i)
-    #     result=googlenews.result()
-    #     df=pd.DataFrame(result)
-
-    filepath_allnews = Path('results/sex_robot_links_'+str(year)+'.csv')   #todo: change file name
+    print(year)
+    filepath_allnews = Path('results/sex_robot/sex_robot_'+str(year)+'.csv')   #todo: change file name
     filepath_allnews.parent.mkdir(parents=True, exist_ok=True)  
-    df.to_csv(filepath_allnews) 
+
+    for month in range(1,13): 
+        
+        start_date = str(month).zfill(2) + '/01/' + str(year)
+        end_day = calendar.monthrange(year, month)[1]
+        end_date = str(month).zfill(2) +'/'+ str(end_day) +'/'+str(year)
+        print(start_date+ " - " + end_date)
+        googlenews=GoogleNews(start=start_date,end=end_date)
+        # googlenews.search('sex robot')
+        googlenews.set_encode('utf-8')
+        googlenews.get_news('sex robot')
+
+        result=googlenews.result()
+        df=pd.DataFrame(result)
+        
+        # for i in range(2,20):
+        #     googlenews.getpage(i)
+        #     result=googlenews.result()
+        #     df=pd.DataFrame(result)
+
+        
+        df.to_csv(filepath_allnews, mode='a', index=False, header=False)         
 
      
